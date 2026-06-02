fcf507db70d389f8f9c8dbffbc292876f6cc71b3
Gia Huy <onggiahuy222@gmail.com>
2026-06-02 17:22:18 +0700

    database migrations

---
+++ DataAccessLayer/Migrations/20260602100710_AddDailyResetTime.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDailyResetTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DailyResetTime",
+                table: "StudentSubscriptions",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "SubscriptionPlans",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "QuestionLimit",
+                value: 9999);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DailyResetTime",
+                table: "StudentSubscriptions");
+
+            migrationBuilder.UpdateData(
+                table: "SubscriptionPlans",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "QuestionLimit",
+                value: 999999);
+        }
+    }
+}
